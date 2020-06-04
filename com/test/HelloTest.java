d98ace77cdc3e92b0fa053d3b38ece6d7ee19059
Suman <sumankrishnaprasad@gmail.com>
2020-06-04 07:06:30 +0530

    Updating Hello Test with new commit from Eclipse

--- com/test/HelloTest.java
+++ com/test/HelloTest.java
@@ -3,6 +3,7 @@ package com.test;
 public class HelloTest {
 	public void method1() {
 		System.out.println("This is a test");
+		System.out.println("This is another line added from eclipse");
 	}
 
 }
